cd3df8227f941a4513479917862462c5d87726a1
Audra <audra.tallman@hotmail.com>
2019-06-12 12:56:00 -0500

    add migrations to add first and last name to user, modify registration form/views to include first and last name

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,11 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:username, :student_name, :student_grade, :subject, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :first_name, :last_name, :student_name, :student_grade, :subject, :email, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.requrie(:user).permit(:username, :student_name, :student_grade, :subject, :email, :password, :password_confirmation)
+    params.requrie(:user).permit(:username, :first_name, :last_name, :student_name, :student_grade, :subject, :email, :password, :password_confirmation)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_154242) do
+ActiveRecord::Schema.define(version: 2019_06_12_174351) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_06_12_154242) do
     t.string "student_name"
     t.integer "student_grade"
     t.string "subject"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
