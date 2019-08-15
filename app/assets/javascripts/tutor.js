6473b2153be23b18d2eb79447e7ee1804b375872
Audra <audra.tallman@hotmail.com>
2019-08-15 10:57:21 -0500

    Modify js and index view files to create/render new tutor form.

--- app/assets/javascripts/tutor.js
+++ app/assets/javascripts/tutor.js
@@ -4,10 +4,59 @@ $(function(){
 })
 
 function enableListeners(){
-  $("button#tutor-requests").on('click', function (event) {
+  $("button#tutor-requests").on('click', function(event) {
     event.preventDefault
     console.log("worked")
     getTutors()
+  })
+  $("button#new-tutor").on('click', function(event) {
+    event.preventDefault
+    console.log("woohoo")
+    newTutor()
+  })
+}
+
+function displayNewForm(){
+  let tutorFormDiv = document.getElementById('new-tutor-form')
+  let html =`
+    <form onsubmit="newTutor(); return false;">
+      <label> Name: </label>
+      <input type="text" id="name"></br>
+      <label> Gender: </label>
+      <input type="text" id="gender"></br>
+      <label> Subject: </label>
+      <input type="text" id="subject"></br>
+      <label> Bio: </label>
+      <input type="text" id="bio"></br>
+      <label> Email: </label>
+      <input type="text" id="email"</br>
+      <input type="submit" value="Add Tutor">
+    </form>
+    `
+}
+
+function newTutor() {
+  const tutor = {
+    name: document.getElementById("name").value,
+    gender: document.getElementById("gender").value,
+    subject: document.getElementById("subject").value,
+    bio: document.getElementById("bio").value,
+    email: document.getElementbyId("email").value
+  }
+  fetch('http://localhost:3000/tutors', {
+    method: 'POST',
+    body: JSON.stringify({ tutor }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  }).then(resp => resp.json())
+  .then(tutor => {
+    tutor =  new Tutor(tutor)
+    $("#all-tutors").html('')
+      document.querySelector("#form").innerHTML += newb
+      let tutorFormDiv = document.getElementById('new-tutor-form');
+      tutorFormDiv.innerHTML = '';
   })
 }
 
@@ -22,9 +71,9 @@ function getTutors(){
       $("#all-tutors").append(tutorsHTML)
       })
       addLinkListener()
-      // $("button#tutor-info").addEventListener("click", function() {alert("Bam")})
     })
 }
+
 function addLinkListener() {
   let links = $("a#tutor-info")
   for (let i = 0; i < links.length; i++) {
@@ -32,25 +81,18 @@ function addLinkListener() {
   }
 }
 
-// function addEventListener(){
-//  $("#show_link").on('click', function (event) {
-//     event.preventDefault
-//     console.log("bazinga")
-//     showTutor()
-//   })
-// }
-
-  function showTutor(event){
-    event.preventDefault()
-    let id = event.currentTarget.dataset.id
-    fetch(`http://localhost:3000/tutors/${id}.json`)
-    .then(res => res.json())
-    .then(tutor => {
-      let newTutor = new Tutor(tutor)
-      let tutorHTML = newTutor.formatShow()
-      $("#info").append(tutorHTML)
-    })
-  }
+function showTutor(event){
+  event.preventDefault()
+  let id = event.currentTarget.dataset.id
+  fetch(`http://localhost:3000/tutors/${id}.json`)
+  .then(res => res.json())
+  .then(tutor => {
+    $("#info-spot").html('')
+    let newTutor = new Tutor(tutor)
+    let tutorHTML = newTutor.formatShow()
+    $("#info-spot").append(tutorHTML)
+  })
+}
 
 class Tutor {
   constructor (tutor) {
@@ -93,16 +135,14 @@ class Tutor {
     // <button id="tutor-info"> Tutor Info
   }
 
-
-
   formatShow() {
     let tutorHTML = `
       <div>
-      <p> Name: ${this.name}</p>
+      <h3> ${this.name}'s Personal Info </h3>
       <p> Email: ${this.email}</p>
       <p> Bio: ${this.bio}</p>
       </div>
       `
-      return tutorHTML
+    return tutorHTML
   }
 }
